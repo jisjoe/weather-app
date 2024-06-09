249bc39cb3f4507c314e822111d81bb4d703833f
Jis Joe Mathew <jisjoemathew@gmail.com>
2024-06-09 12:17:53 +0530

    Define theme colors

---
+++ lib/constants/color_gradients.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColorGradients {
+  static const List<Color> backgroundGradientDay = [
+    Color(0xFF579AE1),
+    Color(0xFF579AE1),
+    Color(0xFF7FB1D3),
+  ];
+
+  static const List<Color> backgroundGradientNight = [
+    Color(0xFF313869),
+    Color(0xFF313869),
+    Color(0xFF525C98),
+  ];
+
+  static List<Color> weatherCard = [
+    const Color(0xFF000915).withOpacity(0.06),
+    const Color(0xFFF7FAFF).withOpacity(0.1),
+  ];
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color glacierBlue = Color(0xFF82ADCA);
+  static const Color white = Color(0xFFF6FBFE);
+  static const Color cornflowerBlue = Color(0xFF579AE1);
+  static const Color rhino = Color(0xFF363C68);
+}
